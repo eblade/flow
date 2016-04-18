c19bf1d3e0279f43f52a042fc4e32706f14f7e45
Johan Egneblad <johan@egneblad.se>
2016-04-18 10:36:23 +0100

    Do not use Tool class for this, read config from ini file mainly

--- flow/__main__.py
+++ flow/__main__.py
@@ -3,9 +3,11 @@
 import sys
 import os
 import ConfigParser
+from argparse import ArgumentParser
 
 from vizone import logging
 from vizone.tool import Tool
+from vizone.client import init
 from vizone.net.message_queue import ConnectionManager
 from vizone.classutils import to_class
 
@@ -16,25 +18,14 @@ from .needs import NeedsStomp, NeedsClient, NeedsStore, NeedsConfig
 from .store import Store
 
 
-# Initialize as "tool" with standard command line args
-tool = Tool('flow', skip_api=False)
-tool.add_argument('profile',
-                  help='path to profile ini file')
-tool.add_argument('--instance-name', '-n', default="default",
-                  help='unique instance name')
-tool.add_argument('--workers', '-w', type=int, default=1,
-                  help='number of workers')
-tool.add_argument('--man', action="store_true",
-                  help="Show man page for the loaded daemon")
-
-def get_stomp(tool, args):
+def get_stomp(username, password, args):
     if hasattr(get_stomp, 'cached'):
         return get_stomp.cached
 
     get_stomp.cached = ConnectionManager(
         "flow_%s" % args.instance_name,
-        username=tool.user,
-        password=tool.password,
+        username=username,
+        password=password,
         heartbeat_interval=args.stomp_heartbeat_interval
             if not args.stomp_disable_heartbeats else None,
         heartbeat_timeout=args.stomp_heartbeat_timeout
@@ -44,9 +35,9 @@ def get_stomp(tool, args):
     return get_stomp.cached
 
 
-def equip(tool, args, client, config, klass, obj):
+def equip(username, password, args, client, config, klass, obj):
     if issubclass(klass, NeedsStomp):
-        stomp = get_stomp(tool, args)
+        stomp = get_stomp(username, password, args)
         obj.set_stomp(stomp)
     if issubclass(klass, NeedsClient):
         obj.set_client(client)
@@ -57,9 +48,53 @@ def equip(tool, args, client, config, klass, obj):
 
 
 if __name__ == '__main__':
-    args, client = tool.init()
+    argparser = ArgumentParser(usage='python -m flow ini-file')
+
+    argparser.add_argument(
+        'profile',
+        help='path to profile ini file')
+    argparser.add_argument(
+        '--instance-name', '-n',
+        help='unique instance name')
+    argparser.add_argument(
+        '--workers', '-w', type=int, default=0,
+        help='number of workers')
+    argparser.add_argument(
+        '--man', action="store_true",
+        help="show man page for the loaded profile")
+    argparser.add_argument(
+        '--debug', '-g', action="store_true",
+        help="log in debug level")
+
+    args = argparser.parse_args()
 
     config = ConfigParser.ConfigParser()
+    
+    # Default config
+    config.add_section('Flow')
+    config.set('Flow', 'app name', 'default')
+    config.set('Flow', 'class', 'path.to.flow.class')
+    config.set('Flow', 'workers', '1')
+
+    config.add_section('Source')
+
+    config.add_section('Logging')
+    config.set('Logging', 'level', 'info')
+    config.set('Logging', 'color', 'yes')
+    config.set('Logging', 'logger', 'terminal')
+    config.set('Logging', 'formal', 'no')
+    config.set('Logging', 'file name', "/tmp/log.txt")
+
+    config.add_section('Viz One')
+    config.set('Viz One', 'enabled', 'yes')
+    config.set('Viz One', 'hostname', 'localhost')
+    config.set('Viz One', 'username', 'user')
+    config.set('Viz One', 'password', 'user')
+    config.set('Viz One', 'use https', 'yes')
+    config.set('Viz One', 'check certificates', 'yes')
+    config.set('Viz One', 'pem file', '')
+    config.set('Viz One', 'time out', '60')
+
     config.read(args.profile)
 
     stomp = None
@@ -68,18 +103,90 @@ if __name__ == '__main__':
     os.chdir(working_dir)
     sys.path.append(working_dir)
     Flow = to_class(config.get('Flow', 'class'))
+    app_name = config.get('Flow', 'app name')
+    workers = args.workers or config.getint('Flow', 'workers')
+    workers = max(workers, 1)
 
     if args.man:
         help(Flow)
         sys.exit(0)
 
+    # Set up Logging
+    logging_debug = config.get('Logging', 'level') == 'debug'
+    logging_color = config.getboolean('Logging', 'color')
+    logging_logger = config.get('Logging', 'logger')
+    logging_formal = config.getboolean('Logging', 'formal')
+    logging_file = config.get('Logging', 'file name')
+
+    if logging_logger == 'terminal':
+        from vizone.logging.terminal import TerminalLogger
+        logger = TerminalLogger()
+        logger.color = logging_color
+        if logging_formal:
+            logger.color = False
+            logger.formal = True
+        logging.set_logger(logger)
+
+    elif args.logger == 'html':
+        from vizone.logging.html import HtmlLogger
+        logger = HtmlLogger()
+        logging.set_logger(logger)
+
+    elif args.logger == 'file':
+        from vizone.logging.file import FileLogger
+        logger = FileLogger(logging_file or os.path.join('/tmp', app_name + '.log'))
+        logging.set_logger(logger)
+
+    logging.get_default_logger().debug = args.debug or logging_debug
+
+    logging.log("Flow settings", {
+        'Application name': app_name,
+        'Main class': Flow.__name__,
+        'Workers': workers,
+        'Working directory': working_dir,
+        'Source class': Flow.SOURCE.__name__,
+    }, 'pp')
+
+    # Set up Viz One Client Instance
+    viz_one_enabled = config.getboolean('Viz One', 'enabled') or 'yes'
+    viz_one_hostname = config.get('Viz One', 'hostname')
+    viz_one_username = config.get('Viz One', 'username')
+    viz_one_password = config.get('Viz One', 'password')
+    viz_one_use_https = config.getboolean('Viz One', 'use https')
+    viz_one_check_certificates = config.get('Viz One', 'check certificates')
+    viz_one_pem_file = config.get('Viz One', 'pem file') or None
+    viz_one_time_out = config.getfloat('Viz One', 'time out')
+
+    logging.log("Viz One Settings", {
+        'enabled': viz_one_enabled,
+        'hostname': viz_one_hostname,
+        'username': viz_one_username,
+        'password': '********' if viz_one_password else None,
+        'use https': viz_one_use_https,
+        'check certificates': viz_one_check_certificates,
+        'pem file': viz_one_pem_file,
+        'time out': viz_one_time_out,
+    }, 'pp')
+
+    if viz_one_enabled:
+        client = init(
+            hostname=viz_one_hostname,
+            user=viz_one_username,
+            password=viz_one_password,
+            instance_name=app_name,
+            secure=viz_one_use_https,
+            verify=viz_one_check_certificates,
+            timeout=viz_one_time_out,
+        )
+    else:
+        client = None
+
+
     source = Flow.SOURCE(**{k.replace(' ', '_'): v for k, v in config.items('Source')})
-    equip(tool, args, client, config, Flow.SOURCE, source)
+    equip(viz_one_username, viz_one_password, args, client, config, Flow.SOURCE, source)
 
     flow = Flow(instance_name=args.instance_name)
-    equip(tool, args, client, config, Flow, flow)
-
-    tool.message_queue = stomp
+    equip(viz_one_username, viz_one_password, args, client, config, Flow, flow)
 
     # Run source.run once, which should call the workers' start method once or
     # more. No parallelisation is done here
@@ -90,25 +197,24 @@ if __name__ == '__main__':
     # Run source.run multiple times as long as there are free workers in the
     # pool. Stop when source.next() raises StopIteration.
     elif issubclass(Flow.SOURCE, Iterable):
-        with Pool(workers=args.workers, join=True) as pool:
+        with Pool(workers=workers, join=True) as pool:
             log_id = LogId()
 
             for obj, info in source:
                 current_log_id = log_id.next()
                 pool.spawn(flow.run, obj, info=info, log_id=current_log_id)
-                logging.debug(
+                logging.info(
                     "(%i) Spawned worker.",
                     current_log_id
                 )
 
             logging.info("Source is out of data.")
 
-
     # Run source.run once and go to an idle loop. Source is typically an
     # event listener of some kind and will call the callback upon 
     # external triggers.
-    else:
-        with Pool(workers=args.workers, join=True) as pool:
+    elif issubclass(Flow.SOURCE, EventBased):
+        with Pool(workers=workers, join=True) as pool:
             log_id = LogId()
 
             def work(obj, info):
@@ -118,4 +224,10 @@ if __name__ == '__main__':
 
             source.callback = work
             source.run()
-            tool.run()
+            while True:
+                time.sleep(1)
+
+    else:
+        raise ValueError(
+            "The Source class must inherit one of Once, Iterable or EventBased"
+        )
