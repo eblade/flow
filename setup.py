960e9ea2aae6c868da3d376893fdf6d19e89a077
Johan Egneblad <johan@egneblad.se>
2016-04-18 11:05:31 +0100

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup
+
+name_ = 'flow'
+version_ = '0.1'
+packages_ = [
+    'flow',
+    'flow.source',
+]
+classifiers_ = [
+    "Development Status :: 5 - Production/Stable",
+    "Intended Audience :: Developers",
+    "Programming Language :: Python :: 2",
+    "Operating System :: MacOS",
+    "Operating System :: Microsoft :: Windows",
+    "Operating System :: POSIX :: Linux",
+] 
+setup(
+    name=name_,
+    version=version_,
+    author='Vizrt',
+    author_email='jegneblad@DELETEMEvizrt.com',
+    description='Daemon and tool framework for the Viz One API',
+    license="Vizrt Confidential",
+    url='https://github.com/eblade/flow',
+    download_url='https://github.com/eblade/flow/archive/master.zip',
+    packages=packages_,
+    classifiers=classifiers_,
+    scripts=[],
+    install_requires=[]
+)
